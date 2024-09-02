b90b4f10460c720f7133289a4d74a7e52743389e
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2024-09-02 10:10:18 -0400

    add new file, DiscountRules.java with constructor

---
+++ src/DiscountRules.java
@@ -0,0 +1,24 @@
+public class DiscountRules {
+	
+	public static void main(String[] args) {
+		
+		Author author = new Author();
+		
+		Book book = new Book(author);
+		book.setValue(59.90);
+		
+		//output
+		System.out.println("Valor: " + book.getValue());
+		
+		if (!book.applyDiscout(0.1)) {
+			System.out.println("Desconto nao pode ser maior que 30!");
+		}else {
+			//depois que e feito o calculo no "value", basta chamar novamente o "value"
+			System.out.println("Valor com desconto: " + book.getValue());
+		}
+		
+		
+		//test
+		book.showDetails();
+	}
+}
